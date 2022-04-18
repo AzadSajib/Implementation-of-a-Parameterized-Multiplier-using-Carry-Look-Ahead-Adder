3aedcb94a835c2b41c789901fffad39a84f680fe
AzadSajib <39127915+AzadSajib@users.noreply.github.com>
2022-04-18 15:01:49 +0600

    Create CLA_floorplan.tcl

---
+++ CLA_floorplan.tcl
@@ -0,0 +1,144 @@
+# Pin
+setPinAssignMode -pinEditInBatch true
+
+# Multicland Pins
+editPin -pin multicand[31] -layer M3 -assign {0 0.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[30] -layer M3 -assign {0 1.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[29] -layer M3 -assign {0 2.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[28] -layer M3 -assign {0 3.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[27] -layer M3 -assign {0 4.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[26] -layer M3 -assign {0 5.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[25] -layer M3 -assign {0 6.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[24] -layer M3 -assign {0 7.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[23] -layer M3 -assign {0 8.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[22] -layer M3 -assign {0 9.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[21] -layer M3 -assign {0 10.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[20] -layer M3 -assign {0 11.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[19] -layer M3 -assign {0 12.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[18] -layer M3 -assign {0 13.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[17] -layer M3 -assign {0 14.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[16] -layer M3 -assign {0 15.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[15] -layer M3 -assign {0 16.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[14] -layer M3 -assign {0 17.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[13] -layer M3 -assign {0 18.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[12] -layer M3 -assign {0 19.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[11] -layer M3 -assign {0 20.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[10] -layer M3 -assign {0 21.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[9] -layer M3 -assign {0 22.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[8] -layer M3 -assign {0 23.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[7] -layer M3 -assign {0 24.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[6] -layer M3 -assign {0 25.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[5] -layer M3 -assign {0 26.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[4] -layer M3 -assign {0 27.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[3] -layer M3 -assign {0 28.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[2] -layer M3 -assign {0 29.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[1] -layer M3 -assign {0 30.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multicand[0] -layer M3 -assign {0 31.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+
+# Multiplier Pins
+editPin -pin multiplier[31] -layer M3 -assign {0 32.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[30] -layer M3 -assign {0 33.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[29] -layer M3 -assign {0 34.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[28] -layer M3 -assign {0 35.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[27] -layer M3 -assign {0 36.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[26] -layer M3 -assign {0 37.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[25] -layer M3 -assign {0 38.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[24] -layer M3 -assign {0 39.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[23] -layer M3 -assign {0 40.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[22] -layer M3 -assign {0 41.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[21] -layer M3 -assign {0 42.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[20] -layer M3 -assign {0 43.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[19] -layer M3 -assign {0 44.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[18] -layer M3 -assign {0 45.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[17] -layer M3 -assign {0 46.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[16] -layer M3 -assign {0 47.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[15] -layer M3 -assign {0 48.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[14] -layer M3 -assign {0 49.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[13] -layer M3 -assign {0 50.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[12] -layer M3 -assign {0 51.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[11] -layer M3 -assign {0 52.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[10] -layer M3 -assign {0 53.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[9] -layer M3 -assign {0 54.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[8] -layer M3 -assign {0 55.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[7] -layer M3 -assign {0 56.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[6] -layer M3 -assign {0 57.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[5] -layer M3 -assign {0 58.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[4] -layer M3 -assign {0 59.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[3] -layer M3 -assign {0 60.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[2] -layer M3 -assign {0 61.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[1] -layer M3 -assign {0 62.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+editPin -pin multiplier[0] -layer M3 -assign {0 63.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+
+# Product Pins
+editPin -pin product[63] -layer M3 -assign {100 0.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[62] -layer M3 -assign {100 1.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[61] -layer M3 -assign {100 2.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[60] -layer M3 -assign {100 3.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[59] -layer M3 -assign {100 4.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[58] -layer M3 -assign {100 5.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[57] -layer M3 -assign {100 6.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[56] -layer M3 -assign {100 7.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[55] -layer M3 -assign {100 8.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[54] -layer M3 -assign {100 9.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[53] -layer M3 -assign {100 10.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[52] -layer M3 -assign {100 11.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[51] -layer M3 -assign {100 12.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[50] -layer M3 -assign {100 13.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[49] -layer M3 -assign {100 14.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[48] -layer M3 -assign {100 15.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[47] -layer M3 -assign {100 16.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[46] -layer M3 -assign {100 17.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[45] -layer M3 -assign {100 18.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[44] -layer M3 -assign {100 19.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin
+editPin -pin product[43] -layer M3 -assign {100 20.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[42] -layer M3 -assign {100 21.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[41] -layer M3 -assign {100 22.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[40] -layer M3 -assign {100 23.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[39] -layer M3 -assign {100 24.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[38] -layer M3 -assign {100 25.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[37] -layer M3 -assign {100 26.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[36] -layer M3 -assign {100 27.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[35] -layer M3 -assign {100 28.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[34] -layer M3 -assign {100 29.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[33] -layer M3 -assign {100 30.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[32] -layer M3 -assign {100 31.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[31] -layer M3 -assign {100 32.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[30] -layer M3 -assign {100 33.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[29] -layer M3 -assign {100 34.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[28] -layer M3 -assign {100 35.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[27] -layer M3 -assign {100 36.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[26] -layer M3 -assign {100 37.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[25] -layer M3 -assign {100 38.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[24] -layer M3 -assign {100 39.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[23] -layer M3 -assign {100 40.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[22] -layer M3 -assign {100 41.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[21] -layer M3 -assign {100 42.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[20] -layer M3 -assign {100 43.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[19] -layer M3 -assign {100 44.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[18] -layer M3 -assign {100 45.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[17] -layer M3 -assign {100 46.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[16] -layer M3 -assign {100 47.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[15] -layer M3 -assign {100 48.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[14] -layer M3 -assign {100 49.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[13] -layer M3 -assign {100 50.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[12] -layer M3 -assign {100 51.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[11] -layer M3 -assign {100 52.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[10] -layer M3 -assign {100 53.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[9] -layer M3 -assign {100 54.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[8] -layer M3 -assign {100 55.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[7] -layer M3 -assign {100 56.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[6] -layer M3 -assign {100 57.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[5] -layer M3 -assign {100 58.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[4] -layer M3 -assign {100 59.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[3] -layer M3 -assign {100 60.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[2] -layer M3 -assign {100 61.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[1] -layer M3 -assign {100 62.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+editPin -pin product[0] -layer M3 -assign {100 63.3} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side E -fixedPin 
+
+
+
+
+#editPin -pin clk -layer M3 -assign {0 8.17} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+#editPin -pin reset -layer M3 -assign {0 8.55} -pinWidth 0.14 -pinDepth 0.28 -fixOverlap false  -side W -fixedPin 
+
+setPinAssignMode -pinEditInBatch false
